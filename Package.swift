3305b389fbac62eb148253edeac57078e56bdee8
leobert <774057695@qq.com>
2023-04-11 17:17:06 +0800

    re package

--- Package.swift
+++ Package.swift
@@ -14,6 +14,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://gitee.com/leobert_253/Resolver", .exact("1.4.5"))
+        
+//        .package(url: "https://github.com/hmlongco/Resolver", .exact("1.4.5")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
